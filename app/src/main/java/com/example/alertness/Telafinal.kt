517b91bf83603e0a88cb9e25d478aa6168f07f38
Renan Gomes <renan.ogomes@gmail.com>
2019-05-06 08:36:37 -0300

    dados sendo salvos em arquivo txt e no firebase database

--- app/src/main/java/com/example/alertness/Telafinal.kt
+++ app/src/main/java/com/example/alertness/Telafinal.kt
@@ -1,11 +1,23 @@
 package com.example.alertness
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.os.SystemClock
 import android.provider.Settings
+import android.view.View
 import kotlinx.android.synthetic.main.activity_pvt.*
 import kotlinx.android.synthetic.main.activity_telafinal.*
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.Writer
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+
 
 var media: Long = 0
 var timestamp: Long = 0
@@ -25,5 +37,32 @@ class Telafinal : AppCompatActivity() {
         textView12.text = "Condição: "+condicao
         textView13.text = "KSS: "+KSS
         textView14.text = "Timestamp: "+ timestamp.toString()
+
+        saveTXT("Identificação: "+id+"; Condição: "+condicao+"; KSS: "+KSS+"; t1: " + t1.toString() + "; t2: " + t2.toString() + "; t3: " + t3.toString() + "; t4: " + t4.toString() + "; t5: " + t5.toString() + "; t6: " + t6.toString() + "; t7: " + t7.toString() + "; t8: " + t8.toString() + "; t9: " + t9.toString() + "; t10: " + t10.toString()+"; Média = "+media.toString()+"; Timestamp: "+ timestamp.toString()+"\n\n")
+
+        savetoDB()
+
+
     }
+
+    private fun savetoDB(){
+
+        val ref = FirebaseDatabase.getInstance().getReference("testes")
+
+        val testeId2 = ref.push().key
+
+        val teste = Teste(testeId2!!, id, condicao, KSS, t1.toString(),t2.toString(), t3.toString(), t4.toString(), t5.toString(), t6.toString(), t7.toString(), t8.toString(), t9.toString(), t10.toString(), media.toString(), timestamp.toString())
+
+        ref.child(testeId2).setValue(teste)
+    }
+
+    private fun saveTXT(TXTString: String) {
+
+        val output: Writer
+        output = BufferedWriter(FileWriter(File(this.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "teste.txt"), true))
+        output.write(TXTString)
+        output.close()
+
+    }
+
 }
---
+++ app/src/main/java/com/example/alertness/Teste.kt
@@ -0,0 +1,3 @@
+package com.example.alertness
+
+class Teste(val id: String, val identificacao: String, val condicao: String, val kss: String, val t1: String, val t2: String, val t3: String, val t4: String, val t5: String, val t6: String, val t7: String, val t8: String,val t9: String, val t10: String, val media: String, val timestamp: String)
